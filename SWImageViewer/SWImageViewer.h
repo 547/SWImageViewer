461eb5507b31343bdc6980f0ac521e9760aad6cc
Seven Wang <seven.wang@supernovachina.com>
2019-06-19 11:02:00 +0800

    Initial Commit

---
+++ SWImageViewer/SWImageViewer.h
@@ -0,0 +1,19 @@
+//
+//  SWImageViewer.h
+//  SWImageViewer
+//
+//  Created by Supernova SanDick SSD on 2019/6/19.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWImageViewer.
+FOUNDATION_EXPORT double SWImageViewerVersionNumber;
+
+//! Project version string for SWImageViewer.
+FOUNDATION_EXPORT const unsigned char SWImageViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWImageViewer/PublicHeader.h>
+
+
